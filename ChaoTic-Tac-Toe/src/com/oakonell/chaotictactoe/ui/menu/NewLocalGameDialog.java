6845560b2fe6564dcb31fc1e2cbe9c6a3c02aa38
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2014-09-06 22:27:41 -0400

    Fix keyboard popping up
    Change from legacy admob id to new id

--- ChaoTic-Tac-Toe/src/com/oakonell/chaotictactoe/ui/menu/NewLocalGameDialog.java
+++ ChaoTic-Tac-Toe/src/com/oakonell/chaotictactoe/ui/menu/NewLocalGameDialog.java
@@ -1,5 +1,6 @@
 package com.oakonell.chaotictactoe.ui.menu;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -120,6 +122,10 @@ public class NewLocalGameDialog extends SherlockDialogFragment {
 
 			@Override
 			public void onClick(View v) {
+				InputMethodManager imm = (InputMethodManager) getActivity()
+						.getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+
 				if (!validate(xNameText, oNameText, frag)) {
 					return;
 				}
